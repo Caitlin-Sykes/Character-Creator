1dbc4aa2918b4fcd99583a54e44a1078c4f137a9
Cait <2423179@dundee.ac.uk>
2023-12-26 16:47:07 +0000

    fixed things, node broke

---
+++ readme.md
@@ -0,0 +1,11 @@
+A random project for all your video game character creator needs.
+
+Currently Supports
+    - Nothing because I haven't built it yet
+
+Uses the technologies:
+    - Python
+    - Electron
+    - HTML/CSS/JS
+    - Tailwind
+    - DaisyUI for Tailwind
---
+++ tailwind.config.js
@@ -0,0 +1,31 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  //Searches for files
+  content: ["./**/*.{html,js,ts}"],
+
+  theme: {
+    extend: {},
+  },
+
+  //Plugins
+  plugins: [require("daisyui")],
+
+  //DaisyUI Config
+  daisyui: {
+    themes: [
+      {
+        //Overrides for Daisy's night theme
+        night: {
+          ...require("daisyui/src/theming/themes")["night"],
+          primary: "#1FE030",
+          secondary: "teal",
+      }
+      
+    },
+
+    ], // false: only light + dark | true: all | array - specific themes
+    darkTheme: "dark", // name of one of the included themes for dark mode
+    themeRoot: "*", // The element that receives theme color CSS variables
+  },
+}
+
