79fc07c9c57712debb66e2db0fff30b6d3dfe712
Cait <2423179@dundee.ac.uk>
2024-02-02 22:07:01 +0000

    its still not working i am much confusion

--- frontend/src/js/html_tabs_switcher.js
+++ frontend/src/js/html_tabs_switcher.js
@@ -1,27 +1,29 @@
 //A function to swap tabs
-// tab = name of the tab to swap to
-function swapTab(tab) {
+// tab = name of the tab to swap to, passed in when onclick is called in html
+// function swapTab(tab) {
+
+    // hi Adam, please ignore this javascript
 
     //Gets and removes active element tag.
-    let active_tabs = document.getElementsByClassName('checked')[0];
-    let active_tab = active_tabs[0];
-    active_tab.classList.remove('checked');
+    // let active_tabs = document.getElementsByClassName('checked');
+    // let active_tab = active_tabs[0];
+    // active_tab.classList.remove('checked');
 
-    //Gets the current active panel by querying the name of the active tab. Hides it
-    let tabs_info = document.getElementById(active_tab.getAttribute('name'))[0];
-    let tab_info = tabs_info[0];
-    tab_info.classList.add('invisible');
+    // //Gets the current active panel by querying the name of the active tab. Hides it
+    // let tabs_info = document.getElementById(active_tab.getAttribute('name'));
+    // let tab_info = tabs_info[0];
+    // tab_info.classList.add('invisible');
 
-    //Gets the tab to be made visible and set to active
-    let new_tabs = document.getElementsByName(tab);
-    let new_tab = new_tabs[0];
+    // //Gets the tab to be made visible and set to active
+    // let new_tabs = document.getElementsByName(tab);
+    // let new_tab = new_tabs[0];
 
-    let new_tabs_info = document.getElementById(tab);
-    let new_tab_info = new_tabs_info[0];
+    // let new_tabs_info = document.getElementById(tab);
+    // let new_tab_info = new_tabs_info[0];
 
 
     //Sets to active tab and makes visible
-    new_tab.classList.add('checked');
-    new_tab_info.remove('invisible');
+    // new_tab.classList.add('checked');
+    // new_tab_info.remove('invisible');
 
-}
+// }
